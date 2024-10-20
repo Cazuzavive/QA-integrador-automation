6bccdd5081d0cbf2495a34dbd1e84fbbf74c0cac
cazuzavive <nefertaritheunique@gmail.com>
2024-10-20 16:00:05 -0300

    casos de prueba funcionando

--- punto3/pages/page_inventory.py
+++ punto3/pages/page_inventory.py
@@ -14,9 +14,8 @@ class Page_Inventory():
         elements = {'onesie':'add-to-cart-sauce-labs-onesie', 'fleece':'add-to-cart-sauce-labs-fleece-jacket', 'backpack': 'add-to-cart-sauce-labs-backpack', 'bike':"add-to-cart-sauce-labs-bike-light", 'bolt':"add-to-cart-sauce-labs-bolt-t-shirt", 'red':"add-to-cart-test.allthethings()-t-shirt-(red)"}
         self.driver.find_element(By.ID,elements[element]).click()
 
-    def sort_element(self.element):
-        select = Select(driver.find_element(By.CLASS_NAME, "product_sort_container"))
-        select.select_by_value("lohi")
+    select = Select(driver.find_element(By.CLASS_NAME, "product_sort_container"))
+    select.select_by_value("lohi")
     
     def go_to_cart(self):
         self.driver.find_element(*self.cart).click()
